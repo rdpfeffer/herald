f731a6ab2a18c018ebf79ba18eb625598374401e
Ryan Pfeffer <ryan.pfeffer@plangrid.com>
2018-12-27 13:31:54 -0800

    Check git statuses

--- script.py
+++ script.py
@@ -1,7 +1,21 @@
 from collections import namedtuple
+import re
 
 StatusLine = namedtuple("StatusLine", ["status", "orig_path", "path"])
 
+UNCHANGED_STATUS_DEMARCATION = " "
+DELETED_STATUSES = set([
+    "D{0}".format(UNCHANGED_STATUS_DEMARCATION),
+    "{0}D".format(UNCHANGED_STATUS_DEMARCATION),
+    "MD",
+    "AD",
+    "RD",
+    "CD",
+    "DD",
+    "UD",
+    "DU",
+])
+UNMERGED_STATUSES = set("DD|AA|UU|AU|UD|UA|DU".split("|"))
 
 def main():
     lines = [
@@ -9,9 +23,17 @@ def main():
         "A  script.py",
         " M script.py",
     ]
+    lines = [l for l in lines if not is_header(l)]
     parsed_lines = [parse(l) for l in lines]
-    checkable_lines = [l for l in parsed_lines if is_checkable(l)]
+    deleted_lines = [l for l in parsed_lines if is_deleted(l.status)]
+    unmerged_lines = [l for l in parsed_lines if is_unmerged(l.status)]
+    checkable_lines = list(
+        set(parsed_lines) - set(deleted_lines) - set(unmerged_lines)
+    )
 
+
+def is_header(line):
+    return line.startswith("#")
 
 def parse(line):
     status = line[0:2]
@@ -30,11 +52,13 @@ def parse(line):
     return StatusLine(status, orig_path, path)
 
 
-def is_checkable(git_status_line):
-    status = {
-        "??": True,
-        "A ": True,
-    }
+def is_deleted(status):
+    return status in DELETED_STATUSES
+
+
+def is_unmerged(status):
+    return status in UNMERGED_STATUSES
+
 
 if __name__ == "__main__":
     main()
--- test_script.py
+++ test_script.py
@@ -1,43 +1,59 @@
+from collections import namedtuple
+
 from script import StatusLine
 from script import parse
+from script import is_deleted
+from script import is_unmerged
+
+Case = namedtuple("Case", ["line", "parsed_line", "deleted", "unmerged"])
+TEST_CASES = [
+    # Statuses without merge conflicts
+    Case(" A a.py", StatusLine(status=" A", orig_path=None, path="a.py"), False, False),
+    Case(" M a.py", StatusLine(status=" M", orig_path=None, path="a.py"), False, False),
+    Case(" D a.py", StatusLine(status=" D", orig_path=None, path="a.py"), True, False),
+    Case("M  a.py", StatusLine(status="M ", orig_path=None, path="a.py"), False, False),
+    Case("MM a.py", StatusLine(status="MM", orig_path=None, path="a.py"), False, False),
+    Case("MD a.py", StatusLine(status="MD", orig_path=None, path="a.py"), True, False),
+    Case("A  a.py", StatusLine(status="A ", orig_path=None, path="a.py"), False, False),
+    Case("AM a.py", StatusLine(status="AM", orig_path=None, path="a.py"), False, False),
+    Case("AD a.py", StatusLine(status="AD", orig_path=None, path="a.py"), True, False),
+    Case("D  a.py", StatusLine(status="D ", orig_path=None, path="a.py"), True, False),
+    Case("R  a.py -> b.py", StatusLine(status="R ", orig_path="a.py", path="b.py"), False, False),
+    Case("RM a.py -> b.py", StatusLine(status="RM", orig_path="a.py", path="b.py"), False, False),
+    Case("RD a.py -> b.py", StatusLine(status="RD", orig_path="a.py", path="b.py"), True, False),
+    Case("C  a.py", StatusLine(status="C ", orig_path=None, path="a.py"), False, False),
+    Case("CM a.py", StatusLine(status="CM", orig_path=None, path="a.py"), False, False),
+    Case("CD a.py", StatusLine(status="CD", orig_path=None, path="a.py"), True, False),
+    Case(" R a.py -> b.py", StatusLine(status=" R", orig_path="a.py", path="b.py"), False, False),
+    Case("DR a.py -> b.py", StatusLine(status="DR", orig_path="a.py", path="b.py"), False, False),
+    Case(" C a.py", StatusLine(status=" C", orig_path=None, path="a.py"), False, False),
+    Case("DC a.py", StatusLine(status="DC", orig_path=None, path="a.py"), False, False),
+    # Statuses with merge conflicts
+    Case("DD a.py", StatusLine(status="DD", orig_path=None, path="a.py"), True, True),
+    Case("AU a.py", StatusLine(status="AU", orig_path=None, path="a.py"), False, True),
+    Case("UD a.py", StatusLine(status="UD", orig_path=None, path="a.py"), True, True),
+    Case("UA a.py", StatusLine(status="UA", orig_path=None, path="a.py"), False, True),
+    Case("DU a.py", StatusLine(status="DU", orig_path=None, path="a.py"), True, True),
+    Case("AA a.py", StatusLine(status="AA", orig_path=None, path="a.py"), False, True),
+    Case("UU a.py", StatusLine(status="UU", orig_path=None, path="a.py"), False, True),
+    # Untracked and Ignored Statuses
+    Case("?? a.py", StatusLine(status="??", orig_path=None, path="a.py"), False, False),
+    Case("!! a.py", StatusLine(status="!!", orig_path=None, path="a.py"), False, False),
+    # Status lines with quoted and escaped paths
+    Case('A  "file with a space.txt"', StatusLine(status="A ", orig_path=None, path='"file with a space.txt"'), False, False),
+    Case(r'A  "file with a space \/ and slash.txt"', StatusLine(status="A ", orig_path=None, path=r'"file with a space \/ and slash.txt"'), False, False),
+]
 
 def test_parse():
-    test_pairs = [
-        # Statuses without merge conflicts
-        (" A a.py", StatusLine(status=" A", orig_path=None, path="a.py")),
-        (" M a.py", StatusLine(status=" M", orig_path=None, path="a.py")),
-        (" D a.py", StatusLine(status=" D", orig_path=None, path="a.py")),
-        ("M  a.py", StatusLine(status="M ", orig_path=None, path="a.py")),
-        ("MM a.py", StatusLine(status="MM", orig_path=None, path="a.py")),
-        ("MD a.py", StatusLine(status="MD", orig_path=None, path="a.py")),
-        ("A  a.py", StatusLine(status="A ", orig_path=None, path="a.py")),
-        ("AM a.py", StatusLine(status="AM", orig_path=None, path="a.py")),
-        ("AD a.py", StatusLine(status="AD", orig_path=None, path="a.py")),
-        ("D  a.py", StatusLine(status="D ", orig_path=None, path="a.py")),
-        ("R  a.py -> b.py", StatusLine(status="R ", orig_path="a.py", path="b.py")),
-        ("RM a.py -> b.py", StatusLine(status="RM", orig_path="a.py", path="b.py")),
-        ("RD a.py -> b.py", StatusLine(status="RD", orig_path="a.py", path="b.py")),
-        ("C  a.py", StatusLine(status="C ", orig_path=None, path="a.py")),
-        ("CM a.py", StatusLine(status="CM", orig_path=None, path="a.py")),
-        ("CD a.py", StatusLine(status="CD", orig_path=None, path="a.py")),
-        (" R a.py -> b.py", StatusLine(status=" R", orig_path="a.py", path="b.py")),
-        ("DR a.py -> b.py", StatusLine(status="DR", orig_path="a.py", path="b.py")),
-        (" C a.py", StatusLine(status=" C", orig_path=None, path="a.py")),
-        ("DC a.py", StatusLine(status="DC", orig_path=None, path="a.py")),
-        # Statuses with merge conflicts
-        ("DD a.py", StatusLine(status="DD", orig_path=None, path="a.py")),
-        ("AU a.py", StatusLine(status="AU", orig_path=None, path="a.py")),
-        ("UD a.py", StatusLine(status="UD", orig_path=None, path="a.py")),
-        ("UA a.py", StatusLine(status="UA", orig_path=None, path="a.py")),
-        ("DU a.py", StatusLine(status="DU", orig_path=None, path="a.py")),
-        ("AA a.py", StatusLine(status="AA", orig_path=None, path="a.py")),
-        ("UU a.py", StatusLine(status="UU", orig_path=None, path="a.py")),
-        # Untracked and Ignored Statuses
-        ("?? a.py", StatusLine(status="??", orig_path=None, path="a.py")),
-        ("!! a.py", StatusLine(status="!!", orig_path=None, path="a.py")),
-        # Status lines with quoted and escaped paths
-        ('A  "file with a space.txt"', StatusLine(status="A ", orig_path=None, path='"file with a space.txt"')),
-        (r'A  "file with a space \/ and slash.txt"', StatusLine(status="A ", orig_path=None, path=r'"file with a space \/ and slash.txt"')),
-    ]
-    for line, parse_example in test_pairs:
+    for line, parse_example, _, _ in TEST_CASES:
         assert parse(line) == parse_example
+
+
+def test_is_deleted():
+    for _, parsed_status, deleted, _ in TEST_CASES:
+        assert is_deleted(parsed_status.status) == deleted
+
+
+def test_is_unmerged():
+    for _, parsed_status, _, unmerged in TEST_CASES:
+        assert is_unmerged(parsed_status.status) == unmerged
