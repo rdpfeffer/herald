17a8455dc7561f768f18b8528dd549be334d4e5e
Ryan Pfeffer <ryan.pfeffer@plangrid.com>
2018-12-28 08:34:43 -0800

    Switch to Porcelain v2 parsing

--- script.py
+++ script.py
@@ -1,21 +1,11 @@
 from collections import namedtuple
-import re
+import shlex
 
-UNCHANGED_STATUS_DEMARCATION = " "
-DELETED_STATUSES = set([
-    "D{0}".format(UNCHANGED_STATUS_DEMARCATION),
-    "{0}D".format(UNCHANGED_STATUS_DEMARCATION),
-    "MD",
-    "AD",
-    "RD",
-    "CD",
-    "DD",
-    "UD",
-    "DU",
-])
+DELETED_STATUSES = set("D.|.D|MD|AD|RD|CD|DD|UD|DU".split("|"))
 UNMERGED_STATUSES = set("DD|AA|UU|AU|UD|UA|DU".split("|"))
+_StatusEntry = namedtuple("StatusEntry", ["status", "submodule", "orig_path", "path"])
 
-_StatusEntry = namedtuple("StatusEntry", ["status", "orig_path", "path"])
+
 class StatusEntry(_StatusEntry):
 
     """Represents one line of status when running 'git status'"""
@@ -25,37 +15,71 @@ class StatusEntry(_StatusEntry):
     def is_unmerged(self):
         return self.status in UNMERGED_STATUSES
 
+    def is_submodule(self):
+        return self.submodule.startswith("S")
 
-def main():
-    lines = [l for l in lines if not is_header(l)]
-    parsed_lines = [parse(l) for l in lines]
-    deleted_lines = [l for l in parsed_lines if l.is_deleted()]
-    unmerged_lines = [l for l in parsed_lines if l.is_unmerged()]
+
+def main(lines):
+    lines = strip_headers(lines)
+    status_entries = parse_lines(lines)
+    deleted_entries = [l for l in status_entries if l.is_deleted()]
+    unmerged_lines = [l for l in status_entries if l.is_unmerged()]
     checkable_lines = list(
-        set(parsed_lines) - set(deleted_lines) - set(unmerged_lines)
+        set(status_entries) - set(deleted_entries) - set(unmerged_lines)
     )
 
 
-def is_header(line):
-    return line.startswith("#")
+def strip_headers(lines):
+    return [l for l in lines if not l.startswith("#")]
+
+def parse_lines(lines):
+    return [parse(l) for l in lines]
 
 
 def parse(line):
-    status = line[0:2]
-    remainder = line[3:]
-    path_parts = remainder.split(" ")
+    status = None
+    submodule = None
     orig_path = None
     path = None
-    try:
-        arrow_separator_idx = path_parts.index("->")
-    except ValueError as e:
-        # The file's path was not changed, so we take all of the remainder
-        path = remainder
-    else:
-        orig_path = " ".join(path_parts[0:arrow_separator_idx])
-        path = " ".join(path_parts[arrow_separator_idx+1:])
-    return StatusEntry(status, orig_path, path)
+    lex = shlex.shlex(line)
+    lex.whitespace_split = True
+    lex.whitespace = " "
+    tokens = list(lex)
+    if tokens[0] in ("!", "?"):
+        return parse_untracked_ignored_entry(tokens)
+    if tokens[0] == "1":
+        return parse_ordinary_change_entry(tokens)
+    if tokens[0] == "2":
+        return parse_renamed_or_copied_entry(tokens)
+    if tokens[0] == "u":
+        return parse_unmerged_entry(tokens)
+    raise ValueError("Line given does not match one of the recognized Git "
+                     "Porcelain V2 formats. {}".format(line))
+
+def parse_untracked_ignored_entry(tokens):
+    # Handle untracked and ignored files, Porcelain V2 gives single letter
+    # status for these. We double them to allow for consistency in string
+    # parsing elsewhere.
+    status, path = tokens
+    return StatusEntry(status * 2, None, None, path)
+
+
+def parse_ordinary_change_entry(tokens):
+    _, status, submodule, _, _, _, _, _, path = tokens
+    return StatusEntry(status, submodule, None, path)
+
+
+def parse_renamed_or_copied_entry(tokens):
+    _, status, submodule, _, _, _, _, _, _, paths = tokens
+    path, orig_path = paths.split("\t")
+    return StatusEntry(status, submodule, orig_path, path)
+
+
+def parse_unmerged_entry(tokens):
+    _, status, submodule, _, _, _, _, _, _, _, path = tokens
+    return StatusEntry(status, submodule, None, path)
 
 
 if __name__ == "__main__":
     main()
+
