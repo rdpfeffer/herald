c8d7926b14876a6abe04f9cff350346313f8002b
Ryan Pfeffer <ryan.pfeffer@plangrid.com>
2019-02-17 22:15:03 -0800

    Add schema validation

---
+++ schema.py
@@ -0,0 +1,40 @@
+from jsonschema import validate
+
+
+def validate_config(instance):
+    """Validate that the config is valid
+
+    """
+    schema = {
+        "$id": "https://example.com/.schema.json",
+        "$schema": "http://json-schema.org/draft-07/schema#",
+        "title": "Projectionist",
+        "description": "A configuration for projections.",
+        "type": "object",
+        "patternProperties": {
+            "(\\\\?([^\\/]*[\\/])*)([^\\/]+)$": {
+                "type": "object",
+                "properties": {
+                    "alternate": {"type": "string"},
+                    "type": {"type": "string"},
+                    "tasks": {
+                        "oneOf": [
+                            {
+                                "type": "object",
+                                "properties": {"parallel": {"type": "array"}},
+                                "additionalProperties": False,
+                            },
+                            {
+                                "type": "object",
+                                "properties": {"serial": {"type": "array"}},
+                                "additionalProperties": False,
+                            },
+                        ]
+                    },
+                },
+                "required": ["type"],
+            }
+        },
+        "additionalProperties": False,
+    }
+    validate(instance=instance, schema=schema)
---
+++ test_schema.py
@@ -0,0 +1,48 @@
+import json
+
+import pytest
+from jsonschema.exceptions import ValidationError
+
+import schema
+
+
+def test_validate_config():
+    example_config = {
+        "src/main/java*.java": {
+            "alternate": "src/test/java/{}.java",
+            "type": "source",
+            "tasks": {"serial": ["ls -la", "echo something"]},
+        },
+        "src/test/java/*.java": {
+            "alternate": "src/main/java/{}.java",
+            "type": "test",
+            "tasks": {"parallel": ["ls -la", "echo something"]},
+        },
+    }
+    schema.validate_config(example_config)
+
+
+def test_validate_bad_pattern():
+
+    with pytest.raises(ValidationError):
+        example_config = {
+            "/": {
+                "alternate": "src/main/java/{}.java",
+                "type": "test",
+                "tasks": {"parallel": ["ls -la", "echo something"]},
+            }
+        }
+        schema.validate_config(example_config)
+
+
+def test_validate_bad_tasks():
+
+    with pytest.raises(ValidationError):
+        example_config = {
+            "src/main/java*.java": {
+                "alternate": "src/main/java/{}.java",
+                "type": "test",
+                "tasks": {"XXXXX": ["ls -la", "echo something"]},
+            }
+        }
+        schema.validate_config(example_config)
