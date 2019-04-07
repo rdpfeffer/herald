766600b85e36fb7b762e625b51d1ae821d2791c6
Ryan Pfeffer <ryan.pfeffer@plangrid.com>
2019-04-07 12:37:50 -0700

    Initial Work on config mapping

---
+++ config.py
@@ -0,0 +1,115 @@
+import fnmatch
+import json
+from itertools import chain, groupby
+from operator import itemgetter
+
+import attr
+
+import executor
+import schema
+import task
+
+
+def load_config(path=".heraldrc.json"):
+    """TODO: Docstring for load_config.
+
+    :path: TODO
+    :returns: TODO
+
+    """
+    with open(path) as file:
+        lines = file.read()
+        data = json.loads(lines)
+        schema.validate(data)
+        return ConfigurationMap(data)
+
+
+@attr.s(frozen=True)
+class MatchPair(object):
+    filepath = attr.ib()
+    task_data = attr.ib()
+
+
+class ConfigurationMap(object):
+
+    """Herald Config"""
+
+    def __init__(self, data):
+        """Initialize config
+
+        :data: TODO
+
+        """
+        self._data = data
+
+    def get_all_task_groups_for_filepaths(self, filepaths):
+        task_groups = []
+        tasks_and_files = chain(
+            *[self.pair_task_entries_with_filepath(filepath) for filepath in filepaths]
+        )
+        for _, group in groupby(tasks_and_files, self._key_for_task_data_entry):
+            group = list(group)
+            pair = group[0]
+            task_data = pair.task_data
+            executor_name, tasks = task_data.copy().popitem()
+            file_group = {pair.filepath for pair in group}
+            task_groups.append(
+                self._task_group_for_task_entry(executor_name, task_data, file_group)
+            )
+        return task_groups
+
+    def pair_task_entries_with_filepath(self, filepath):
+        task_data_entries = self._map_filepath_to_task_data_entries(filepath)
+        return [MatchPair(filepath, entry) for entry in task_data_entries]
+
+    @staticmethod
+    def _key_for_task_data_entry(entry_pair):
+        return str(entry_pair.task_data)
+
+    def _map_filepath_to_task_data_entries(self, filepath):
+        return [
+            entry["tasks"]
+            for _, entry in self._map_filepath_to_config_entries(filepath)
+        ]
+
+    def _map_filepath_to_config_entries(self, filepath):
+        return [
+            (pattern, self._data[pattern])
+            for pattern in self._data
+            if fnmatch.fnmatch(filepath, pattern)
+        ]
+
+    def _task_group_for_task_entry(self, executor_name, task_data, filepaths):
+        executor = self._executor_for_name(executor_name)
+        tasks = task_data[executor_name]
+        return task.TaskGroup(executor, tasks, filepaths)
+
+    def _executor_for_name(self, executor_name):
+        if executor_name == "parallel":
+            return executor.ParallelExecutor()
+        elif executor_name == "serial":
+            return executor.SerialExecutor()
+        else:
+            raise InternalExecutorError()
+
+    def get_test_alternates_for_filepath(self, filepath):
+        alternate_paths = [
+            self._compute_alternate(pattern, entry["alternate"], filepath)
+            for pattern, entry in self._map_filepath_to_config_entries(filepath)
+            if "alternate" in entry
+        ]
+
+    def _compute_alternate(self, pattern, alternate, filepath):
+        """Compute the alternate of a given filepath.
+
+       This takes everything between the first and last special character, trimming off either end, and using that as input to the alternate format string."""
+        beggining = pattern[0 : self._first_special_char()]
+        end = pattern[self._last_special_char() :]
+        middle = filepath[len(beggining) : -len(end)]
+        return alternate.format(middle)
+
+
+class InternalExecutorError(Exception):
+
+    """Thrown when a bad execution mode is supplied. If this error is ever thrown,
+    then then there is some internal error with the program"""
---
+++ test_config.py
@@ -0,0 +1,86 @@
+import pytest
+
+import config
+import executor
+import task
+
+
+@pytest.fixture
+def basic_config():
+    return config.ConfigurationMap(
+        {
+            "src/main/java/*.java": {
+                "alternate": "src/test/java/{}.java",
+                "type": "source",
+                "tasks": {"serial": ["ls -la", "echo something"]},
+            },
+            "src/test/java/*.java": {
+                "alternate": "src/main/java/{}.java",
+                "type": "test",
+                "tasks": {"parallel": ["ls -la", "echo something"]},
+            },
+        }
+    )
+
+
+def test_executor_creation_serial():
+    config_map = config.ConfigurationMap({})
+    serial_exec = config_map._executor_for_name("serial")
+    assert isinstance(serial_exec, executor.SerialExecutor)
+
+
+def test_executor_creation_parallel():
+    config_map = config.ConfigurationMap({})
+    serial_exec = config_map._executor_for_name("parallel")
+    assert isinstance(serial_exec, executor.ParallelExecutor)
+
+
+def test_executor_creation_internal_error():
+    config_map = config.ConfigurationMap({})
+    with pytest.raises(config.InternalExecutorError):
+        serial_exec = config_map._executor_for_name("bananas")
+
+
+def test_task_group_for_task_entry():
+    config_map = config.ConfigurationMap({})
+    task_group = config_map._task_group_for_task_entry(
+        "parallel", {"parallel": ["foo"]}, "a/b/c.txt"
+    )
+    assert isinstance(task_group, task.TaskGroup)
+
+
+def test_map_filepath_to_config_entries(basic_config):
+    entries = basic_config._map_filepath_to_config_entries("src/main/java/foo.java")
+    assert entries == [
+        (
+            "src/main/java/*.java",
+            {
+                "alternate": "src/test/java/{}.java",
+                "type": "source",
+                "tasks": {"serial": ["ls -la", "echo something"]},
+            },
+        )
+    ]
+
+
+def test_map_filepath_to_task_data_entries(basic_config):
+    entries = basic_config._map_filepath_to_task_data_entries("src/main/java/foo.java")
+    assert entries == [{"serial": ["ls -la", "echo something"]}]
+
+
+def test_get_all_task_groups_for_filepaths(basic_config):
+    task_groups = basic_config.get_all_task_groups_for_filepaths(
+        ["src/main/java/foo.java"]
+    )
+    assert len(task_groups) == 1
+    for task_group in task_groups:
+        assert task_group._tasks == ("ls -la", "echo something")
+
+def test_alternate_matching(basic_config):
+    assert False
+
+def test_alternative_test_group_creation():
+    assert False
+
+def test_more_complex_case_with_many_files():
+    assert False
