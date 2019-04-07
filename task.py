0cdb0a6b7f70f98e90f9cdf873f3a7d6ace8c484
Ryan Pfeffer <ryan.pfeffer@plangrid.com>
2019-04-06 20:58:43 -0700

    Create TaskGroup

---
+++ task.py
@@ -0,0 +1,19 @@
+import attr
+
+import executor
+
+
+@attr.s(frozen=True)
+class TaskGroup(object):
+
+    """A group of tasks to be run"""
+
+    _executor = attr.ib(validator=attr.validators.instance_of(executor.Executor))
+    _tasks = attr.ib(converter=tuple)
+    _filepaths = attr.ib(converter=frozenset)
+
+    def copy_with_additional_paths(self, filepaths):
+        return TaskGroup(self._executor, self._tasks, self._filepaths.union(filepaths))
+
+    def run(self, filepaths):
+        self._executor.run(self._tasks, filepaths)
---
+++ test_task.py
@@ -0,0 +1,36 @@
+import attr
+import pytest
+
+import executor
+import task
+
+
+def test_immutable_properties():
+    g = task.TaskGroup(executor.ParallelExecutor(), [], [])
+    with pytest.raises(attr.exceptions.FrozenInstanceError):
+        g._executor = True
+    with pytest.raises(attr.exceptions.FrozenInstanceError):
+        g._tasks = True
+    with pytest.raises(attr.exceptions.FrozenInstanceError):
+        g._filepaths = True
+
+
+def test_copy_additional_paths():
+    g = task.TaskGroup(executor.ParallelExecutor(), [], ["x/y/bar.txt"])
+    g2 = g.copy_with_additional_paths(["a/b/foo.txt"])
+    assert id(g2) != id(g)
+    assert g2._filepaths  ==  frozenset(["a/b/foo.txt", "x/y/bar.txt"])
+
+
+def test_run():
+    g = task.TaskGroup(executor.ParallelExecutor(), [], ["x/y/bar.txt"])
+    g.run("bar.txt")
+
+
+def test_hashable_by_value():
+    p = executor.ParallelExecutor()
+    g1 = task.TaskGroup(p, [], ["x/y/bar.txt"]),
+    g2 = task.TaskGroup(p, [], ["x/y/bar.txt"]),
+    s = {g1, g2}
+    assert hash(g1) == hash(g2)
+    assert len(s) == 1
